a99e61de4d7a9c1eaff340f58f9a7099238eee7b
eqeyildirim <eqeyildirims@gmail.com>
2024-11-26 22:57:45 -0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+//       Grocery grocery=new Grocery();
+//       grocery.startGrocery();
+        MobilePhone mob=new MobilePhone("32132",new ArrayList<>());
+        mob.addNewContact(new Contact("Ali","32132"));
+        mob.addNewContact(new Contact("ADSI","321321"));
+        mob.addNewContact(new Contact("adsi","321321"));
+        mob.addNewContact(new Contact("dedsa","321321"));
+        mob.addNewContact(new Contact(null,"321321"));
+        mob.printContact();
     }
 }
